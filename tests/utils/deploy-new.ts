0ba1d2b5cafa1c7df0a8c0cb93c6c257f75356d0
bjmrq <bmarquiscom@gmail.com>
2022-08-29 19:34:05 +0200

    merge related tasks

--- tests/utils/deploy-new.ts
+++
@@ -1,8 +0,0 @@
-import { Contract } from "ethers"
-import { ethers } from "hardhat"
-
-export const deployNewContract = async <TContract extends Contract>(contractName: string) => {
-  const Contract = await ethers.getContractFactory(contractName)
-
-  return (await Contract.deploy()) as TContract
-}

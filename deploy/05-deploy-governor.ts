3db378bb74ac188d59b369914e087b082eaa6fa1
bjmrq <bmarquiscom@gmail.com>
2022-09-02 14:18:14 +0200

    Add more info to governor

--- deploy/05-deploy-governor.ts
+++ deploy/05-deploy-governor.ts
@@ -1,13 +1,15 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 import { simpleContractDeployAndVerify } from "../helpers/contracts/deploy"
+import { sleep } from "../helpers/chain/wait-block"
 
 const deployColorBox: DeployFunction = async ({}: HardhatRuntimeEnvironment) => {
+  await sleep(40000)
   await simpleContractDeployAndVerify({
     contractName: "DummyGovernor",
     deploymentArguments: [],
     deploymentOptions: {
-      gasLimit: "8000000",
+      gasLimit: "10000000",
     },
   })
 }
